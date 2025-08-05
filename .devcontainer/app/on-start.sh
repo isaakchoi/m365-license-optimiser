7c2580cee54cca759cfbd05f32cd93b1eeef031a
ChoiBoiii <Isaakchoi@gmail.com>
2025-08-05 06:18:38 +0000

    Add initial devcontainer

---
+++ .devcontainer/app/on-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# DESCRIPTION
+# This script is executed on container startup (not during compilation)
+# See '.devcontainer/devcontainer.json'
+# It is not copied to the container, rather the version included in the mount of this repository is executed
